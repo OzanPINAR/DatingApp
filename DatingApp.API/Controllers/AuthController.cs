b4d105ba603ae18972cf1c3c207802a4cae7a96a
ozanpinar <ozanpinar@ozanpinar-MacBook-Pro.local>
2018-12-09 22:01:50 +0300

    Photo upload functionality

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
@@ -18,71 +19,77 @@ namespace DatingApp.API.Controllers
     {
         private readonly IAuthRepository _repo;
         private readonly IConfiguration _config;
-        public AuthController(IAuthRepository repo, IConfiguration config)
+        private readonly IMapper _mapper;
+        public AuthController(IAuthRepository repo, IConfiguration config,
+        IMapper mapper)
         {
             _config = config;
             _repo = repo;
+            _mapper = mapper;
         }
 
-        [HttpPost("register")]
-        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+    {
+        // validated request
+        userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+
+        if (await _repo.UserExists(userForRegisterDto.Username))
         {
-            // validated request
-            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
-
-            if (await _repo.UserExists(userForRegisterDto.Username))
-            {
-                return BadRequest("Username already exists");
-            }
-
-            var userToCreate = new User
-            {
-                Username = userForRegisterDto.Username
-            };
-
-            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
-
-            // fix later
-            return StatusCode(201);
+            return BadRequest("Username already exists");
         }
 
-        [HttpPost("login")]
-        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        var userToCreate = new User
         {
-            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+            Username = userForRegisterDto.Username
+        };
 
-            if (userFromRepo == null)
-            {
-                return Unauthorized();
-            }
-            var claims = new[]
-            {
+        var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
+
+        // fix later
+        return StatusCode(201);
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+    {
+        var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+
+        if (userFromRepo == null)
+        {
+            return Unauthorized();
+        }
+        var claims = new[]
+        {
                 new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
                 new Claim(ClaimTypes.Name, userFromRepo.Username)
             };
 
-            var key = new SymmetricSecurityKey(Encoding.UTF8
-                .GetBytes(_config.GetSection("AppSettings:Token").Value));
+        var key = new SymmetricSecurityKey(Encoding.UTF8
+            .GetBytes(_config.GetSection("AppSettings:Token").Value));
 
-            // token signing
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+        // token signing
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
 
-            // token creation
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(claims),
-                Expires = DateTime.Now.AddDays(1),
-                SigningCredentials = creds
-            };
+        // token creation
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.Now.AddDays(1),
+            SigningCredentials = creds
+        };
 
-            var tokenHandler = new JwtSecurityTokenHandler();
+        var tokenHandler = new JwtSecurityTokenHandler();
 
-            var token = tokenHandler.CreateToken(tokenDescriptor);
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        var user = _mapper.Map<UserForListDto>(userFromRepo);
 
             return Ok(new
             {
-                token = tokenHandler.WriteToken(token)
+                token = tokenHandler.WriteToken(token),
+                user
             });
-        }
     }
+}
 }
