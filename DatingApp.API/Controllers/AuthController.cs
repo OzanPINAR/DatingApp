ce213ec4e131ba5c0c7b0d775eedb5869319963d
ozanpinar <ozanpinar@ozanpinar-MacBook-Pro.local>
2018-12-10 23:51:25 +0300

    Improved registration

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -39,15 +39,15 @@ namespace DatingApp.API.Controllers
             return BadRequest("Username already exists");
         }
 
-        var userToCreate = new User
-        {
-            Username = userForRegisterDto.Username
-        };
+        var userToCreate = _mapper.Map<User>(userForRegisterDto);
 
         var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
 
+        var userToReturn = _mapper.Map<UserForDetailedDto>(createdUser);
+
         // fix later
-        return StatusCode(201);
+        return CreatedAtRoute("GetUser", new {controller = "Users", id = createdUser.Id},
+        userToReturn);
     }
 
     [HttpPost("login")]
